aee461543fe8acb0f30e4814df94d02e8533d8d6
Jack the Pug <jack@watchpug.org>
2022-01-21 08:32:12 +0800

    use `close` to repay after maturity

--- YieldLev.sol
+++ YieldLev.sol
@@ -39,12 +39,14 @@ contract YieldLever {
   bytes6 constant ilkId = bytes6(0x303900000000); // for yvUSDC
   IToken constant iUSDC = IToken(0x32E4c68B3A4a813b710595AebA7f6B7604Ab9c15); 
   IERC20 constant USDC = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+  address constant USDCJoin = address(0x0d9A1A773be5a83eEbda23bf98efB8585C3ae4f4);
   YieldLadle constant Ladle = YieldLadle(0x6cB18fF2A33e981D1e38A663Ca056c0a5265066A);
   address constant yvUSDCJoin = address(0x403ae7384E89b086Ea2935d5fAFed07465242B38);
 
   bytes4 private constant BUILD_SELECTOR = bytes4(keccak256("build(bytes6,bytes6,uint8)"));
   bytes4 private constant SERVE_SELECTOR = bytes4(keccak256("serve(bytes12,address,uint128,uint128,uint128)"));
   bytes4 private constant REPAY_SELECTOR = bytes4(keccak256("repayVault(bytes12,address,int128,uint128)"));
+  bytes4 private constant CLOSE_SELECTOR = bytes4(keccak256("close(bytes12,address,int128,int128)"));
 
   address internal immutable dev;
 
@@ -94,21 +96,47 @@ contract YieldLever {
     address pool, // get your pool address from the address of the "Trade" event emited in the invest() tx
     int128 ink // same, get it from the invest() tx, make it negative since we are going to withdraw here
   ) external {
-    iUSDC.flashBorrow(
+    if (maxAmount != 0) {
+      iUSDC.flashBorrow(
         maxAmount,
         address(this),
         address(this),
         "",
-        abi.encodeWithSignature("doUnwind(bytes12,address,int128)", vaultId, pool, ink)
-    );
+        abi.encodeWithSignature("doRepay(bytes12,address,int128)", vaultId, pool, ink)
+      );
+    } else {
+      iUSDC.flashBorrow(
+        uint256(uint128(-ink)),
+        address(this),
+        address(this),
+        "",
+        abi.encodeWithSignature("doClose(bytes12,address,int128)", vaultId, pool, ink)
+      );
+    }
   }
+    
 
-  function doUnwind(bytes12 vaultId, address pool, int128 ink) external {
+  function doRepay(bytes12 vaultId, address pool, int128 ink) external {
     uint256 borrowAmount = USDC.balanceOf(address(this));
     USDC.transfer(pool, borrowAmount);
     // repay yield
     bytes[] memory ladleCallData = new bytes[](1);
     ladleCallData[0] = abi.encodeWithSelector(REPAY_SELECTOR, vaultId, address(this), ink, borrowAmount); // REPAY
+    Ladle.batch(ladleCallData);
+    // withdraw from yvUSDC
+    yvUSDC.withdraw();
+    // repay
+    USDC.transfer(address(iUSDC), borrowAmount);
+    // send to user
+    USDC.transfer(dev, USDC.balanceOf(address(this)));
+  }
+
+  function doClose(bytes12 vaultId, address pool, int128 ink) external {
+    uint256 borrowAmount = USDC.balanceOf(address(this));
+    USDC.approve(USDCJoin, borrowAmount);
+    // repay yield
+    bytes[] memory ladleCallData = new bytes[](1);
+    ladleCallData[0] = abi.encodeWithSelector(CLOSE_SELECTOR, vaultId, address(this), ink, ink); // CLOSE
     Ladle.batch(ladleCallData);
     // withdraw from yvUSDC
     yvUSDC.withdraw();
